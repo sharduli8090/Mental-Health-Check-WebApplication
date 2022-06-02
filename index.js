bf26422f295c84709e9cc275d162f9c4358d1b65
Sharduli  Pandey <shardulipandey8090@gmail.com>
2022-06-02 17:51:06 +0530

    Added Responsiveness

--- index.js
+++ index.js
@@ -22,8 +22,17 @@ var set6=["How often have your loved one been bothered by trouble failing or sta
 
 
 function shown(){
-	var disc = '<div id="main2" style="margin:8%;padding-right: 2%;padding-left: 2%;padding-top: 3%;padding-bottom: 1%;background-color: #ffffff62;"><figure class="text-center"><blockquote class="blockquote"><p class="display-7">Health is Wealth! <br>Often we have heard this statement that if you are healthy then no one can stop you from being wealthy. We always follow this statement but often we forget that health is not sticked to physical health only, but this is not at all true, mental health is also important. Infact, mental health is the most important part and cant be ignored at any cost! Sometimes we dont even get to know that we our suffering from this disease. Also when people get to know about this disease they try to hide this, because of society fears. But its completely normal or ok to have depression or similars. Its like any other disease, which needs to be detected at earlier stages to avoid any hazardous conditions! For that we first need to do some screening tests at our level.This is a small test designed for self screening of depression. It hardly takes less than 10 minutes to complete this test but can become a life saviour for you or your closed ones! Its an anonymous test and data is asked just for evaluation purpose only. If you are 16 or above you can take this test!</p></blockquote><figcaption class="blockquote-footer" >Creativstan<cite title="Source Title"> By Sharduli Pandey</cite></figcaption><div class="alert alert-primary d-flex align-items-center" role="alert"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">		<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>	  </svg><div>This is just a small self-screening and to get the best treatment, Doctor Consultation is advisable!</div></div><a class="btn btn-outline-warning btn-lg" href="info.html" role="button" data-bs-toggle="tooltip" data-bs-placement="top" title="Click here to start the screening test!">Start The Test</a></figure></div>';
-	document.getElementById('all').innerHTML = disc;
+    document.getElementById('box').style.display="none";
+	var disc = "<div id='disclamairHead'>Health is Wealth! </div>"+
+    "<div id='descriptionText'>"+
+    "Often we have heard this statement that if you are healthy then no one can stop you from being wealthy. We always follow this statement but often we forget that health is not stuck to physical health only, but this is not at all true, mental health is also important. Mental health is the most important part and can't be ignored at any cost! Sometimes we don't even get to know that we are suffering from this disease. Also when people get to know about this disease they try to hide this, because of society's fears. But it's completely normal or ok to have depression or similar. It's like any other disease, which needs to be detected at earlier stages to avoid any hazardous conditions! For that, we first need to do some screening tests at our level. This is a small test designed for self-screening of depression. It hardly takes less than 10 minutes to complete this test but can become a life savior for you or your close ones! It is an anonymous test and data is asked just for evaluation purposes only. If you are 16 or above you can take this test!"+
+    '</div>'+
+    '<div id="footerDisclamair">Creativstan By <i>Sharduli Pandey</i></div>'+
+    '<div id="warningText"><i class="fa fa-exclamation-circle" style="font-size:30px"></i>'+
+    '<div >This is just a small self-screening and to get the best treatment, Doctor Consultation is advisable!</div></div>'+
+    '<button id="startText" class="mainBtn" title="Click Here To Start The Test" onclick="startTheTest()">Start The Test</button>';
+	document.getElementById('box2').innerHTML = disc;
+    document.getElementById('box2').style.display="flex";
 }
 
 
@@ -63,33 +72,33 @@ function always(num){
 	}
 }
 function setOne(dig){
-	var template = '<blockquote class="blockquote"><p class="display-6">'+set1[dig]+'</p></blockquote>';
-	document.getElementById('ques').innerHTML = template;
+	var template = '<div class="question">'+set1[dig]+'</div>';
+	document.getElementById('box2').innerHTML = template;
 	return;
 }
 function setTwo(dig){
-	var template = '<blockquote class="blockquote"><p class="display-6">'+set2[dig]+'</p></blockquote>';
-	document.getElementById('ques').innerHTML = template;
+	var template = '<div class="question">'+set2[dig]+'</div>';
+	document.getElementById('box2').innerHTML = template;
 	return;
  }
 function setThree(dig){
-	var template = '<blockquote class="blockquote"><p class="display-6">'+set3[dig]+'</p></blockquote>';
-	document.getElementById('ques').innerHTML = template;
+	var template = '<div class="question">'+set3[dig]+'</div>';
+	document.getElementById('box2').innerHTML = template;
 	return;
  }
  function setFour(dig){
-	var template = '<blockquote class="blockquote"><p class="display-6">'+set4[dig]+'</p></blockquote>';
-	document.getElementById('ques').innerHTML = template;
+	var template ='<div class="question">'+set4[dig]+'</div>';
+	document.getElementById('box2').innerHTML = template;
 	return;
  }
  function setFive(dig){
-	var template = '<blockquote class="blockquote"><p class="display-6">'+set5[dig]+'</p></blockquote>';
-	document.getElementById('ques').innerHTML = template;
+	var template = '<div class="question">'+set5[dig]+'</div>';
+	document.getElementById('box2').innerHTML = template;
 	return;
  }
  function setSix(dig){
-	var template = '<blockquote class="blockquote"><p class="display-6">'+set6[dig]+'</p></blockquote>';
-	document.getElementById('ques').innerHTML = template;
+	var template = '<div class="question">'+set6[dig]+'</div>';
+	document.getElementById('box2').innerHTML = template;
 	return;
  }
 
@@ -113,9 +122,8 @@ function showQues(j,n){
 	else if(n==6){
 		setSix(j);
 	}
-	var buttonShow = '<div class="d-grid gap-2">'+'<button class="btn btn-primary" type="button"  onclick="never('+n+')">Never</button>'+'<button class="btn btn-primary" type="button" onclick="some('+n+')">Sometimes</button>'+'<button class="btn btn-primary" type="button" onclick="half('+n+')">Half Of The Time</button>'+'<button class="btn btn-primary" type="button" onclick="always('+n+')">Everytime</button>'+'</div>';
-	document.getElementById('ques').innerHTML += buttonShow;
-
+	var buttonShow = '<div id="buttonQuesFlex">'+'<button class="buttonOption" onclick="never('+n+')">Never</button>'+'<button class="buttonOption"  onclick="some('+n+')">Sometimes</button>'+'<button class="buttonOption" onclick="half('+n+')">Half Of The Time</button>'+'<button class="buttonOption b" onclick="always('+n+')">Everytime</button>'+'</div>';
+	document.getElementById('box2').innerHTML += buttonShow;
 }
 
 function selfs(val){
@@ -154,28 +162,28 @@ if(lossVar==1){
 }
 if(ageVar==1){
 	if(selfVar==1){
-		document.getElementById('ques').innerHTML = "";
+		document.getElementById('box2').innerHTML = "";
 		showQues(i,1);
 	}else if(selfVar==2){
-		document.getElementById('ques').innerHTML = "";
+		document.getElementById('box2').innerHTML = "";
 		showQues(i,2);
 	}
 }
 else if(ageVar==2){
 	if(selfVar==1){
-		document.getElementById('ques').innerHTML = "";
+		document.getElementById('box2').innerHTML = "";
 		showQues(i,3);
 	}else if(selfVar==2){
-		document.getElementById('ques').innerHTML = "";
+		document.getElementById('box2').innerHTML = "";
 		showQues(i,4);
 	}
 }
 else if(ageVar==3){
 	if(selfVar==1){
-		document.getElementById('ques').innerHTML = "";
+		document.getElementById('box2').innerHTML = "";
 		showQues(i,5);
 	}else if(selfVar==2){
-		document.getElementById('ques').innerHTML = "";
+		document.getElementById('box2').innerHTML = "";
 		showQues(i,6);
 	}
 }
@@ -183,44 +191,96 @@ else if(ageVar==3){
 
 }
 
+function reload(){
+	location.reload();
+}
 
 function endMsg(advise){
-	var temp = '<div class="alert alert-success" role="alert" style="text-align:left;">  <h4 class="alert-heading">Score After Evaluation : '+score+' </h4>  <p>'+advise+'</p>  <hr>  <p class="mb-0">Happy Mental Health </p></div><div class="alert alert-danger d-flex align-items-center" role="alert"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">		<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>	  </svg><div>This is just a small self-screening and to get the best treatment, Doctor Consultation is advisable!</div></div><br><a class="btn btn-outline-dark btn-lg" href="info.html" role="button" data-bs-toggle="tooltip" data-bs-placement="top" title="Click here to start the screening test again!">Take The Test Again</a><span> </span><a class="btn btn-outline-dark btn-lg" href="First Page.html" role="button" data-bs-toggle="tooltip" data-bs-placement="top" title="Click here to go to home page!">Go To Home Page</a>';
-	document.getElementById('ques').innerHTML += temp;
-	
+	var temp = '<div class="alertFlex">'
+	+'<div id="scoreWhole">'
+	+'<div id="score"> Score After Evaluation : '+score+' </div>'
+	+'<div id="advise">'+advise+'</div>'
+	+'</div>'
+	+'  <hr>'
+	+'  <div id="wish">Happy Mental Health </div>'
+	+'<div id="warningText"><i class="fa fa-exclamation-circle" style="font-size:30px"></i>'
+	+'<div>This is just a small self-screening and to get the best treatment, Doctor Consultation is advisable!</div></div>'
+	+'<button id="startNew" class="mainBtn " title="Click Here To Start The Test" onclick="startTheTest()">Take Test Again</button>'
+	+'<button onclick="reload()" title="Click here to go to home page!" class="mainBtn" id="goHomePageBtn">Go To Home Page</button>';
+	document.getElementById('box2').innerHTML += temp;	
 }
 
 function results(){
 	alert('You have successfully answered all the questions!!')
-    document.getElementById('ques').innerHTML = "";
+    document.getElementById('box2').innerHTML = "";
 	var templates;
 	if(score>=40){
-		templates = '<div class="alert alert-danger" role="alert"><strong>You are at a very High Risk !</strong></div>';
-		document.getElementById('ques').innerHTML += templates;
+		templates = '<div class="alertHead" id="veryhighAlert">You are at a very High Risk !</div>';
+		document.getElementById('box2').innerHTML += templates;
 		endMsg('Doctor consultation is highly recommended that too ASAP! Talk with your family! And remember you are not alone!');
 	}
 	else if(score>=27 && score<40){
-		templates = '<div class="alert alert-danger" role="alert"><strong>You are at a High Risk !</strong></div>';
-		document.getElementById('ques').innerHTML += templates;
+		templates = '<div class="alertHead" id="highAlert">You are at a High Risk !</div>';
+		document.getElementById('box2').innerHTML += templates;
 		endMsg('Doctor Consultation is advisable! Talk with your family! And remember you are not alone!');
 		
 	}
 	else if(score>=16 && score<27){
-		templates = '<div class="alert alert-primary" role="alert"><strong>You are at a Medium Risk !</strong></div>';
-		document.getElementById('ques').innerHTML += templates;
+		templates = '<div class="alertHead" id="mediumAlert">You are at a Medium Risk !</div>';
+		document.getElementById('box2').innerHTML += templates;
 		endMsg('Talk with your Family! You can consult your Doctor if any of the above symptoms continues or become frequent!');
 		
 	}
 	else if(score>=6 && score<16){
-		templates = '<div class="alert alert-primary" role="alert"><strong>You are at Less Risk !</strong></div>';
-		document.getElementById('ques').innerHTML += templates;
+		templates = '<div class="alertHead" id="lessAlert">You are at Less Risk !</div>';
+		document.getElementById('box2').innerHTML += templates;
 		endMsg('Your Mental is doing well! Just believe in yourself and never forget that you are not alone! Talk to your loved ones if needed!');
 		
 	}
 	else if(score>=0 && score<6){
-		templates = '<div class="alert alert-success" role="alert"><strong>	You are at a very Low Risk !</strong> </div>';
-		document.getElementById('ques').innerHTML += templates;
+		templates = '<div class="alertHead" id="veryLessAlert">You are at a very Low Risk ! </div>';
+		document.getElementById('box2').innerHTML += templates;
 		endMsg("Your Mental Health is completely fine!! KEEP GOING HAPPILY!! ");
 		
 	}
+}
+
+function startTheTest(){
+	shown();
+    document.getElementById('box2').innerHTML = "";
+    var infoTemp = '<div id="infoFlex">'+
+    '<div id="headInfo">Please provide the primary info</div>'+
+    '<div id="optionOne" class="options">'+
+    '<select class="optional" onchange="selfs(this)">'+
+      '<option selected>You are taking this test for</option>'+
+      '<option value="1">Yourself</option>'+
+      '<option value="2">Your Loved One</option>'+
+      '</select>'+
+      '<br>'+
+      '<select class="optional" onchange="ages(this)">'+
+        '<option selected>Your Age</option>'+
+        '<option value="1">Between 16 to 35</option>'+
+        '<option value="2">Between 36 to 50</option>'+
+        '<option value="3">Above 50</option>'+
+      '</select>'+
+      '<br>'+
+      '<select class="optional" onchange="gens(this)">'+
+       '<option selected>Your Gender</option>'+
+        '<option value="1">Male</option>'+
+        '<option value="2">Female</option>'+
+        '<option value="3">Other</option>'+
+      '</select>'+
+      '<br>'+
+      '<div class="labelLoss">Any mishappening has happened to you or your loved one like the loss of anyone close, any financial crisis, any relationship issues, etc.</div>'+
+      '<select id="lossOption" class="optional" onchange="loss(this)">'+
+        '<option selected>Select</option>'+
+        '<option value="1">Yes</option>'+
+        '<option value="2">No</option>'+
+      '</select>'+
+  '<br>'+
+    '</select>'+
+    '</div>'+
+    '<button id="nextTest" onclick="start()">NEXT</button>'
+    '</div>';
+    document.getElementById('box2').innerHTML = infoTemp;
 }
